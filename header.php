e5abf6ca0c83199d07ea96fad9309df893795e1e
Ben Kaminski <ben@benkaminski.com>
2021-05-03 17:02:51 -0400

    initial WP commit

---
+++ header.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+<?php get_template_part( 'parts/page', 'header-tracking' ); ?>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<?php get_template_part( 'parts/page', 'favicons' ); ?>
+<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<?php get_template_part( 'parts/page', 'main-nav' ); ?>
