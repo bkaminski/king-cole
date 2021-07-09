6620e3908db608cb5ba21b5e61f6d716ed071b3b
Ben Kaminski <ben@benkaminski.com>
2021-07-09 18:54:43 -0400

    added new imagery, favicon, and loading sequence

--- parts/home-about.php
+++ parts/home-about.php
@@ -1,9 +1,14 @@
-<div class="container">
-	<div class="front-page-text text-center">
-		<img src="<?php echo get_home_url();?>/wp-content/uploads/kc-logo-white.png" class="img-fluid pb-3" alt="King Cole Farm Logo" width="500" />
-		<h1 class="text-center text-offwhite serif-heading" style="font-size: 5.5rem;">Invites You</h1>
-		<p class="text-offwhite"><em>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a gallev</em></p>
-		<p class="text-offwhite pb-3"><em>Opening our doors to your memorable occasion.</em></p>
-		<a href="<?php echo get_home_url(); ?>/welcome-king-cole-farm" class="btn btn-green btn-lg px-5">Learn More</a>
+<div class="container pt-5">
+	<div class="row pt-5">
+		<div class="col-lg-6 text-center pt-5">
+			<div class="pt-5">
+				<img src="<?php echo get_home_url();?>/wp-content/uploads/kingcole.png" class="img-fluid pt-5 pb-3" alt="King Cole Farm Logo" width="500" />
+				<h1 class="serif-heading" style="font-size: 5.5rem;">Invites You</h1>
+				<p><em>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a gallev</em></p>
+				<p class="pb-3"><em>Opening our doors to your memorable occasion.</em></p>
+				<a href="<?php echo get_home_url(); ?>/welcome-king-cole-farm" class="btn btn-green btn-lg px-5">Learn More</a>
+			</div>
+		</div>
+		<div class="col-lg-6"></div>
 	</div>
 </div>
